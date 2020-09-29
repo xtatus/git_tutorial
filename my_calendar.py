5f7ed2ca331f4d5a4de4b68ceaf7569ebe216476
saitovitor <saitovitor@hotmail.com>
2020-09-29 16:08:16 -0300

    patch tests

--- my_calendar.py
+++ my_calendar.py
@@ -1,9 +1,10 @@
-import unittest
-from requests.exceptions import Timeout
-from unittest.mock import Mock
+import requests
+from datetime import datetime
 
-# Mock requests to control its behavior.
-requests = Mock()
+def is_weekday():
+    today = datetime.today()
+    # Python's datetime library treats Monday as 0 and Sunday as 6.
+    return (0 <= today.weekday() < 5)
 
 def get_holidays():
     r = requests.get('http://localhost/api/holidays')
@@ -11,24 +12,4 @@ def get_holidays():
         return r.json()
     return None
 
-class TestCalendar(unittest.TestCase):
-    def test_get_holidays_retry(self):
-        # Create a new Mock to imitate a Response.
-        response_mock = Mock()
-        response_mock.status_code = 200
-        response_mock.json.return_value = {
-            '12/25':'Christmas',
-            '7/4':'Independence Day',
-        }
-        # Set the side effect of .get()
-        requests.get.side_effect = [Timeout, response_mock]
-        # Test that the first request raises a Timeout.
-        with self.assertRaises(Timeout):
-            get_holidays()
-        # Now retry, expecting a successful response.
-        assert get_holidays()['12/25'] == 'Christmas'
-        # Finally, assert .get() was called twice
-        assert requests.get.call_count == 2
 
-if __name__ == '__main__':
-    unittest.main()

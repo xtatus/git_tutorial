23cebb1c91a39f8aaa96a6a9e6e444ba68e8eb14
saitovitor <saitovitor@hotmail.com>
2020-09-29 13:53:54 -0300

    v.14

--- my_calendar.py
+++ my_calendar.py
@@ -1,18 +1,9 @@
-import datetime
-import requests
+import unittest
+from requests.exceptions import Timeout
 from unittest.mock import Mock
 
-# Save a couple of test days.
-tuesday = datetime.datetime(year=2019, month=1, day=1)
-saturday = datetime.datetime(year=2019, month=1, day=5)
-
-# Mock datetime to control today's date.
-datetime = Mock()
-
-def is_weekday():
-    today = datetime.datetime.today()
-    # Python's datetime library treats Monday as 0 and Sunday as 6.
-    return (0 <= today.weekday() and today.weekday() < 5)
+# Mock requests to control its behaviour
+requests = Mock()
 
 def get_holidays():
     r = requests.get('http://localhost/api/holidays')
@@ -20,14 +11,12 @@ def get_holidays():
         return r.json()
     return None
 
-# Mock .today() to return Tuesday.
-datetime.datetime.today.return_value = tuesday
+class TestCalendar(unittest.TestCase):
+    def test_get_holidays_timeout(self):
+        # Test a connection timeout
+        requests.get.side_effect = Timeout
+        with self.assertRaises(Timeout):
+            get_holidays()
 
-# Test Tuesday is a weekday.
-assert is_weekday()
-
-# Mock .today() to return Saturday.
-datetime.datetime.today.return_value = saturday
-
-# Test Saturday is not a weekday.
-assert not is_weekday()
+if __name__ == '__main__':
+    unittest.main()

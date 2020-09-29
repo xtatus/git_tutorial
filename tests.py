21301c549b6671ce7ce444e9bab6ae3d6a6f2916
saitovitor <saitovitor@hotmail.com>
2020-09-29 16:52:17 -0300

    patching an object's attributes

--- tests.py
+++ tests.py
@@ -1,15 +1,13 @@
 import unittest
-from my_calendar import get_holidays
+from my_calendar import get_holidays, requests
 from requests.exceptions import Timeout
 from unittest.mock import patch
 
 class TestCalendar(unittest.TestCase):
-    def test_get_holidays_timeout(self):
-        with patch('my_calendar.requests') as mock_requests:
-            mock_requests.get.side_effect = Timeout
-            with self.assertRaises(Timeout):
+    @patch.object(requests, 'get', side_effect=requests.exceptions.Timeout)
+    def test_get_holidays_timeout(self, mock_requests):
+        with self.assertRaises(requests.exceptions.Timeout):
                 get_holidays()
-                mock_requests.get.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()

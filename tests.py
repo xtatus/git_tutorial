039636862f453e8ac9da8f8b40d53497b754f880
saitovitor <saitovitor@hotmail.com>
2020-09-29 16:44:04 -0300

    patch() as a Context Manager

--- tests.py
+++ tests.py
@@ -4,12 +4,12 @@ from requests.exceptions import Timeout
 from unittest.mock import patch
 
 class TestCalendar(unittest.TestCase):
-    @patch('my_calendar.requests')
-    def test_get_holidays_timeout(self, mock_requests):
-        mock_requests.get.side_effect = Timeout
-        with self.assertRaises(Timeout):
-            get_holidays()
-            mock_requests.get.assert_called_once()
+    def test_get_holidays_timeout(self):
+        with patch('my_calendar.requests') as mock_requests:
+            mock_requests.get.side_effect = Timeout
+            with self.assertRaises(Timeout):
+                get_holidays()
+                mock_requests.get.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
